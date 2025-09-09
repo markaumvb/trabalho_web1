40cf6afe7cf413f3b80bf905dfde91bccffda152
Marcus Vinicius Bertoncello <markaumvb@gmail.com>
2025-09-08 21:46:58 -0300

    ok

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -5,11 +5,10 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { AlunoModule } from './aluno/aluno-module';
 import { DisciplinaModule } from './disciplina/disciplina-module';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [App],
-  imports: [BrowserModule, AppRoutingModule, AlunoModule, DisciplinaModule, FontAwesomeModule],
+  imports: [BrowserModule, AppRoutingModule, AlunoModule, DisciplinaModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
 })
